583ae18031421bbff5876981f87334af9cde69cb
orzehhowski <filmyminecraftnayt@gmail.com>
2025-11-08 19:30:22 +0100

    simple dice pip recognition

--- src/dice_recognition/opencv.py
+++ src/dice_recognition/opencv.py
@@ -11,61 +11,75 @@ ASPECT_RATIO_DELTA = 0.2
 MIN_DICE_AREA = 5000
 MAX_DICE_AREA = 100000
 
+MIN_PIP_RADIUS = 8
+MAX_PIP_RADIUS = 30
+
 INPUT_DATA_DIR = "data/input"
 OUTPUT_DATA_DIR = "data/output"
 
-# Merge overlapping boxes
-def merge_boxes(boxes):
-    merged = []
-    for b in boxes:
-        x1, y1, x2, y2 = b
-        aspect_ratio = abs(x2 - x1) / abs(y2 - y1)
-        merged_flag = False
-        # try merging only if aspect ratio doesn't match the expected
-        if abs(aspect_ratio - EXPECTED_ASPECT_RATIO) > ASPECT_RATIO_DELTA:
-            for i, (mx1, my1, mx2, my2) in enumerate(merged):
-                aspect_ratio_merged = abs(mx2 - mx1) / abs(my2 - my1)
-                if abs(aspect_ratio_merged - EXPECTED_ASPECT_RATIO) > ASPECT_RATIO_DELTA:
-                    if not (x2 < mx1 or x1 > mx2 or y2 < my1 or y1 > my2):  # overlap
-                        merged[i] = (min(x1,mx1), min(y1,my1), max(x2,mx2), max(y2,my2))
-                        merged_flag = True
-                        break
-                    
-        if not merged_flag:
-            merged.append(b)
-    return merged
+# detect pips inside the box, returning them as list of tuples: (abs_x, abs_y, r)
+def pip_detector(img, box, min_r, max_r):
+	x1, y1, x2, y2 = box
+
+	range = img[y1:y2, x1:x2]
+	
+	circles = cv2.HoughCircles(
+		range,
+		cv2.HOUGH_GRADIENT_ALT,
+		dp=1.2,
+		minDist=2 * min_r,
+		param1=300,
+		param2=0.7,
+		minRadius=min_r,
+		maxRadius=max_r
+	)
+
+	detected_circles = []
+	if circles is not None:
+			circles = np.uint16(np.around(circles))
+			for (cx, cy, r) in circles[0, :]:
+					# Convert to original image coordinates
+					abs_cx, abs_cy = x1 + cx, y1 + cy
+					detected_circles.append((abs_cx, abs_cy, r))
+
+	return detected_circles
 
 def process_data():
-    for i in range(1, len(os.listdir(INPUT_DATA_DIR)) + 1):
-        # Load image
-        img = cv2.imread(f'{INPUT_DATA_DIR}/{i}.jpg')
-        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	for i in range(1, len(os.listdir(INPUT_DATA_DIR)) + 1):
+		# Load image
+		img = cv2.imread(f'{INPUT_DATA_DIR}/{i}.jpg')
+		img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        # Preprocess
-        img_blur = cv2.GaussianBlur(img_gray, (5, 5), 0)
-        img_thresh = cv2.adaptiveThreshold(img_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 25, 8)
-        
-        # closing edges - removing small missing parts
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
-        img_closed = cv2.morphologyEx(img_thresh, cv2.MORPH_CLOSE, kernel)
+		# Preprocess
+		img_blur = cv2.GaussianBlur(img_gray, (5, 5), 0)
+		img_thresh = cv2.adaptiveThreshold(img_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 25, 8)
+		
+		# closing edges - removing small missing parts
+		kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
+		img_closed = cv2.morphologyEx(img_thresh, cv2.MORPH_CLOSE, kernel)
 
-        cv2.imwrite(f"{OUTPUT_DATA_DIR}/preprocessed{i}.jpg", img_closed)
+		cv2.imwrite(f"{OUTPUT_DATA_DIR}/preprocessed{i}.jpg", img_closed)
 
-        # Find contours (dice)
-        contours, _ = cv2.findContours(img_closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+		# Find contours (dice)
+		contours, _ = cv2.findContours(img_closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        boxes = []
-        for cnt in contours:
-            x, y, w, h = cv2.boundingRect(cnt)
-            area = w * h
-            if MIN_DICE_AREA < area < MAX_DICE_AREA:
-                boxes.append((x, y, x+w, y+h))
+		boxes = []
+		for cnt in contours:
+			x, y, w, h = cv2.boundingRect(cnt)
+			area = w * h
+			if MIN_DICE_AREA < area < MAX_DICE_AREA:
+				boxes.append((x, y, x+w, y+h))
 
-        merged_boxes = merge_boxes(boxes)
 
-        for box in merged_boxes:
+		for box in boxes:
+			pips = pip_detector(img_closed, box, MIN_PIP_RADIUS, MAX_PIP_RADIUS)
+			
+			# Draw on original image
+			for pip in pips:
+				# Draw the circle and center
+				abs_cx, abs_cy, r = pip
+				cv2.circle(img, (abs_cx, abs_cy), r, (0, 255, 0), 2)
+				#cv2.circle(img, (abs_cx, abs_cy), 2, (0, 0, 255), 3)
+			cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2)
 
-            # Draw on original image
-            cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2)
-
-        cv2.imwrite(f"{OUTPUT_DATA_DIR}/{i}.jpg", img)
+		cv2.imwrite(f"{OUTPUT_DATA_DIR}/{i}.jpg", img)
